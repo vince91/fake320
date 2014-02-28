a183fc257ad9103815ba0fd040c2081e89d260cb
Vincent Timofti <vincent@dhcpwifi-23-39.enst.fr>
2014-02-28 10:33:41 +0100

    Initial Commit

---
+++ Fake320/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Fake320
+//
+//  Created by Vincent Timofti on 28/02/2014.
+//  Copyright (c) 2014 Vincent Timofti. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
